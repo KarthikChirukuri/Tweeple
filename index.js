33f0eec275090b126102e94ac1afc720c667557b
KarthikChirukuri <karthik.chirukuri2005@gmail.com>
2025-09-17 23:30:35 +0530

    Added Authentication signUp and logIn

--- index.js
+++ index.js
@@ -1,35 +1,65 @@
+require("dotenv").config();
+
 const express = require("express");
 const app = express();
 const path = require("path");
 const port = 3000;
-const { v4: uuidv4 } = require('uuid');
-const methodOverride = require('method-override');
-const mongoose = require('mongoose');
+const { v4: uuidv4 } = require("uuid");
+const methodOverride = require("method-override");
+const mongoose = require("mongoose");
 const Post = require("./models/post.js");
+const User = require("./models/user.js");
+
+const dbUrl = process.env.ATLASDB_URL;
+
+const cookieParser = require("cookie-parser");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcryptjs");
+const { error } = require("console");
+const secret = "mySecretKey";
+app.use(cookieParser());
 
 main()
-.then((res)=>{
+  .then((res) => {
     console.log("connection successful");
-})
-.catch(err => console.log(err));
+  })
+  .catch((err) => console.log(err));
 
 async function main() {
-  await mongoose.connect('mongodb://127.0.0.1:27017/Tweeple');
+  await mongoose.connect(dbUrl);
 }
 
-app.use(methodOverride('_method'));
+app.use(methodOverride("_method"));
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static('public'));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static("public"));
+app.use(express.static(path.join(__dirname, "public")));
 
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 
-app.set("view engine","ejs");
-app.set("views", path.join(__dirname,"views"));
+async function attachUser(req, res, next) {
+  const token = req.cookies.token;
+  if (!token) {
+    req.user = null;
+    return next();
+  }
+
+  try {
+    const decoded = jwt.verify(token, secret);
+    const user = await User.findById(decoded.id).select("username");
+    req.user = user; // Now req.user has { _id, username }
+  } catch (err) {
+    req.user = null;
+  }
+  next();
+}
+
+app.use(attachUser);
 
 // let posts = [{
 //     id: uuidv4(),
 //     title: "Post 1",
-//     content: "Telugu is found recorded as early as the 7th century AD but as a literary language it came into its own probably in the 11th century when Nannaya translated the Mahabharata into this language. In the period 500-1100, Telugu was confined to the poetic works and flourished in the courts of kings and among scholars. This period also saw the translation of Ganitasara, a mathematical treatise of Mahivaracharya, into Telugu by Pavuluri Mallana." 
+//     content: "Telugu is found recorded as early as the 7th century AD but as a literary language it came into its own probably in the 11th century when Nannaya translated the Mahabharata into this language. In the period 500-1100, Telugu was confined to the poetic works and flourished in the courts of kings and among scholars. This period also saw the translation of Ganitasara, a mathematical treatise of Mahivaracharya, into Telugu by Pavuluri Mallana."
 //     }, {
 //         id: uuidv4(),
 //         title: "Post 2",
@@ -49,93 +79,130 @@ app.set("views", path.join(__dirname,"views"));
 //     }
 // ];
 
-app.get("/", async (req,res)=>{
-    let posts = await Post.find();
-    // console.log(posts);
-    res.render("index.ejs",{posts});
+app.get("/login", (req, res) => {
+  res.render("login.ejs");
 });
 
-app.get("/post/new", (req, res)=>{
-    console.log("Printing /posts/new");
-    const {title} = req.query;
-    res.render("new.ejs",{title});
+app.post("/login", async (req, res) => {
+  let { username, password } = req.body;
+  const user = await User.findOne({ username });
+  if (!user) return res.status(400).json({ error: "User Not Found!" });
+
+  const isMatch = await bcrypt.compare(password, user.password);
+  if (!isMatch) return res.status(400).json({ error: "Invalid credentials" });
+
+  const token = jwt.sign({ id: user._id }, secret, { expiresIn: "1h" });
+  res.cookie("token", token, {
+    httpOnly: true,
+    secure: false,
+    maxAge: 60 * 60 * 1000,
+  });
+  res.redirect("/");
 });
 
-app.post("/posting", (req,res)=>{
-    let { title, content } = req.body;
-    let id = uuidv4();
-    console.log(req.body);
-    // posts.push({id, title,content});
-    let newPosts = new Post({
-        title: title,
-        content: content
+app.get("/signUp", (req, res) => {
+  res.render("signUp.ejs");
+});
+
+app.post("/signUp", async (req, res) => {
+  let { username, password, confirmPassword } = req.body;
+  const Existinguser = await User.findOne({ username });
+  if (Existinguser) return res.json({ error: "User Exists" });
+
+  if (password != confirmPassword) {
+    return res.status(400).json({ error: "Passwords do not match" });
+  }
+
+  const hashedPassword = await bcrypt.hash(password, 10);
+  const newUser = new User({ username, password: hashedPassword });
+  await newUser.save();
+  res.redirect("/login");
+});
+
+app.get("/", async (req, res) => {
+  let posts = await Post.find();
+  // console.log(posts);
+  res.render("index.ejs", { posts, currentUser: req.user });
+});
+
+app.get("/post/new", (req, res) => {
+  console.log("Printing /posts/new");
+  const { title } = req.query;
+  res.render("new.ejs", { title });
+});
+
+app.post("/posting", (req, res) => {
+  let { title, content } = req.body;
+  let id = uuidv4();
+  console.log(req.body);
+  // posts.push({id, title,content});
+  let newPosts = new Post({
+    title: title,
+    content: content,
+  });
+
+  newPosts
+    .save()
+    .then((res) => {
+      console.log("Saved succesfully");
+    })
+    .catch((err) => {
+      console.log(err);
     });
 
-    newPosts.save()
-        .then((res)=>{
-            console.log("Saved succesfully");
-        })
-        .catch((err)=>{
-            console.log(err);
-        })
-
-    // console.log(newPosts);
-    res.redirect("/");
+  // console.log(newPosts);
+  res.redirect("/");
 });
 
-app.get("/post/:id", async (req,res)=>{
-    let { id }= req.params;
-    let post = await Post.findById(id);
-    if (!post) {
-        return res.status(404).send("Post not found");
-    }
-    // console.log(post);
-    res.render("showUser.ejs",{post});
+app.get("/post/:id", async (req, res) => {
+  let { id } = req.params;
+  let post = await Post.findById(id);
+  if (!post) {
+    return res.status(404).send("Post not found");
+  }
+  // console.log(post);
+  res.render("showUser.ejs", { post });
 });
 
-app.patch("/post/:id", async(req, res)=>{
-    let { id } = req.params;
-    let {content} = req.body;
-    let updatedPost = await Post.findByIdAndUpdate(id,
-        {content},
-        {runValidators: true, new: true}
-    );
-    // let newContent = req.body.content;
-    // let post = posts.find(post => post.id === id);
-    // post.content = newContent;
-    console.log(updatedPost);
-    // res.redirect(`/post/${id}`);
-    // res.send("Updated");
-    res.redirect(`/`);
-    // res.redirect("/post/:id");
+app.patch("/post/:id", async (req, res) => {
+  let { id } = req.params;
+  let { content } = req.body;
+  let updatedPost = await Post.findByIdAndUpdate(
+    id,
+    { content },
+    { runValidators: true, new: true }
+  );
+  // let newContent = req.body.content;
+  // let post = posts.find(post => post.id === id);
+  // post.content = newContent;
+  console.log(updatedPost);
+  // res.redirect(`/post/${id}`);
+  // res.send("Updated");
+  res.redirect(`/`);
+  // res.redirect("/post/:id");
 });
 
-
-app.get("/post/:id/edit", async (req,res)=>{
-    let { id } = req.params;
-    // let post = posts.find(post => post.id === id);
-    let post = await Post.findById(id);
-    console.log(post);
-    res.render("edit.ejs",{post});
+app.get("/post/:id/edit", async (req, res) => {
+  let { id } = req.params;
+  // let post = posts.find(post => post.id === id);
+  let post = await Post.findById(id);
+  console.log(post);
+  res.render("edit.ejs", { post });
 });
 
-app.delete("/post/:id", async (req,res)=>{
-    let { id } = req.params;
-    console.log(id);
-    // posts = posts.filter(post => post.id !== id);
-    let deletedPost = await Post.findByIdAndDelete(id);
-    console.log(deletedPost);
-    res.redirect("/");
+app.delete("/post/:id", async (req, res) => {
+  let { id } = req.params;
+  console.log(id);
+  // posts = posts.filter(post => post.id !== id);
+  let deletedPost = await Post.findByIdAndDelete(id);
+  console.log(deletedPost);
+  res.redirect("/");
 });
 
-app.get("/subscription",(req,res)=>{
-    res.render("subscription.ejs");
+app.get("/subscription", (req, res) => {
+  res.render("subscription.ejs");
 });
 
-app.get("/login",(req,res)=>{
-    res.render("login.ejs");
-});
-
-app.listen(port, ()=>{
-    console.log("Server is running on port " + port);
+app.listen(port, () => {
+  console.log("Server is running on port " + port);
 });
